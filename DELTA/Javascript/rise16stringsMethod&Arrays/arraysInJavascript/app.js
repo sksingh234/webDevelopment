7e4abcbc18ad9cd55e50cd1c6a401aae558e8010
sksingh234 <shubhamkumar95817@gmail.com>
2024-09-24 23:54:33 +0530

    learning array methods

---
+++ DELTA/Javascript/rise16stringsMethod&Arrays/arraysInJavascript/app.js
@@ -0,0 +1,46 @@
+// how to write arrays
+
+// let students = ["ram" , "shyam" , "mohan"];
+// console.log(students);
+// console.log( typeof students);
+// console.log( students[0] [0]);
+
+// // arrays method
+// // push pop unshift shift
+
+// let students = ["ram" , "shyam" , "mohan"];
+
+// students.push("mobin");
+// console.log(students);
+// students.push("shaikh");
+// console.log(students);
+
+// students.pop();
+// console.log(students);
+// students.pop();
+// console.log(students);
+
+// students.unshift("mobin");
+// console.log(students);
+// students.unshift("shaikh");
+// console.log(students);
+
+// students.shift();
+// console.log(students);
+// students.shift();
+// console.log(students);
+
+
+// concat and reverse method
+
+let primary = ["red", "blue"];
+let secondary = ["white", "green"];
+
+let allColors = primary.concat(secondary);
+console.log(allColors);
+
+console.log(allColors.reverse());
+
+console.log(allColors);
+console.log(primary);
+console.log(secondary);
--- DELTA/Javascript/rise16stringsMethod&Arrays/practiceQs/app.js
+++ DELTA/Javascript/rise16stringsMethod&Arrays/practiceQs/app.js
@@ -1,12 +1,26 @@
-// Qs 01
+// practice sheet 01
 
-let msg = "   help!";
-console.log(msg.trim().toUpperCase());
+// // Qs 01
 
-// Qs 02
+// let msg = "   help!";
+// console.log(msg.trim().toUpperCase());
 
-let name = "apnacollege";
-console.log(name.slice(4,9));
-console.log(name.indexOf("na"));
-console.log(name.replace("apna","our"));
-console.log(name.slice(4).replace("l","t"));
+// // Qs 02
+
+// let name = "apnacollege";
+// console.log(name.slice(4,9));
+// console.log(name.indexOf("na"));
+// console.log(name.replace("apna","our"));
+// console.log(name.slice(4).replace("l","t"));
+
+
+// practice sheet 02
+
+let arr = ["january", "july" , "march", "august"];
+
+arr.shift();
+arr.shift();
+arr.unshift("june");
+arr.unshift("july");
+
+console.log(arr);
