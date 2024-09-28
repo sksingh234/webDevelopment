b38e9dbe35e13e7ddf2bca8707b36e46db6a27d0
sksingh234 <shubhamkumar95817@gmail.com>
2024-09-28 13:41:40 +0530

    solved assignment of javascript basics 03 day 16

--- DELTA/Javascript/rise16stringsMethod&Arrays/assignment/app.js
+++ DELTA/Javascript/rise16stringsMethod&Arrays/assignment/app.js
@@ -3,18 +3,48 @@
  For example: for array [7, 9, 0,-2] and n=3
  Print, [7, 9, 0]     */
 
-let num = [7,9,0,-2];
-console.log(num.slice(0,3));
-let size = num.length;
-console.log( num.size);
-console.log(num.slice(size-3));
+// let num = [7,9,0,-2];
+// console.log(num.slice(0,3));
+
+// let size = num.length;
+// console.log(size);
+// console.log(num.slice(size-3));
 
 // Write a JavaScript program to check whether a string is blank or not.
 
-// let str = "";
-// if(str === false){
+// let str = "ram";
+// if(str.length == 0){
 //     console.log("It is a blank string.");
 // }
 // else{
 //     console.log("It is not a blank string.");
 // }
+
+/*    Qs4. Write a JavaScript program to test whether the character at the given (character)
+ index is lower case.    */
+
+// let str = "shubHam";
+// let idx = 4;
+
+// if(str[idx] == str[idx].toLowerCase()){
+//     console.log(`The character at idx ${idx} is in lowercase.`);
+// }
+// else{
+//     console.log(`The character at idx ${idx} is not in lowercase.`);
+// }
+
+//  Qs5. Write a JavaScript program to trim leading and trailing spaces from a string
+// let str = "       shubham     ";
+// console.log(`The string before the trim is ${str}`);
+// console.log(`The string after the trim is ${str.trim()}`);
+
+
+// Qs6. Write a JavaScript program to check if an element exists in an array or not
+
+let arr = ["a", "bbccd"];
+if(arr.length == 0){
+    console.log(`The arr = ${arr} is empty.`);
+}
+else{
+    console.log(`The arr = ${arr} is not empty.`);
+}
