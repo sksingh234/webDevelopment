e83988fe6c4da6583fc2d02054ea42e67501c92d
sksingh234 <shubhamkumar95817@gmail.com>
2024-09-30 21:10:30 +0530

    solving assignment(basics literals) 05 of javascript

---
+++ DELTA/Javascript/rise18objectsLiterals/assignment05ObjectLiterals/app.js
@@ -0,0 +1,40 @@
+/*
+
+Qs1. Create a program that generates a random number representing a dice roll.
+[The number should be between 1 and 6].
+*/
+
+// let random = Math.floor(Math.random() * 6) + 1;
+// console.log(random);
+
+
+/*
+Qs2. Create an object representing a car that stores the following properties for the
+car: name, model, color.
+Print the car’s name.
+*/
+
+// let car = {
+//     name : "Buggati",
+//     model: "F11",
+//     color: ["red", "yellow", "pink"],
+// };
+// console.log(car.name);
+
+/*
+Qs3. Create an object Person with their name, age and city.
+Edit their city’s original value to change it to “New York”.
+Add a new property country and set it to the United States.
+*/
+
+// let person = {
+//     name : "Buggati",
+//     age : 52,
+//     city : "Mumbai",
+// };
+// console.log(person.city);
+
+// person.city = "Patna";
+
+// console.log(person.city);
+
