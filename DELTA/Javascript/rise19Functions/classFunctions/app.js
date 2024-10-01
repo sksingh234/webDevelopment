59ee3413e1fea4d1f833a65bd945e3f149b3aac1
sksingh234 <shubhamkumar95817@gmail.com>
2024-10-01 21:10:25 +0530

    learning about javascript functions

---
+++ DELTA/Javascript/rise19Functions/classFunctions/app.js
@@ -0,0 +1,66 @@
+// Practics question 01
+
+// create a function that print a poem.
+
+// function poem(){
+//     console.log("Twinkle Twinkle little star");
+//     console.log("how are wonder what's you are");
+//     console.log("up above the world so high");
+//     console.log("like a diamond in the sky");
+// }
+
+// poem();
+
+// Practics question 02
+
+// create a function to roll a dice and always show a number b/w 1 to 6.
+
+// function rollDice(){
+//     console.log(Math.floor(Math.random() * 6) + 1);
+// }
+
+// rollDice();
+
+// Practics question 03
+
+// create a function that gives us a average of three numbers.
+
+// function avg(a,b,c){
+//     console.log(`The average is ${ (a+b+c) / 3}`);
+// }
+
+// avg(1,2,3);
+
+// create a function that print the table.
+
+// function table(num){
+//     for(let i = num; i <= num * 10; i += num){
+//         console.log(i);
+//     }
+// }
+
+// table(4);
+
+// create a function that return the sum of integers from 1 to n.
+
+// function sum(n){
+//     let sum = 0;
+//     for (let i = 1; i <= n; i++){
+//         sum += i;
+//     }
+//     return sum;
+// }
+// let addition = sum(5);
+// console.log(addition);
+
+// create a function for concatination of string
+
+// function concat(str){
+//     let result = "";
+//     for(let i = 0; i < str.length; i++){
+//        result += str[i];
+//     }
+//     return result;
+// }
+// let strs = ["ram", "aam", "khata", "hai."];
+// console.log(concat(strs));
