9a07acbfca3f7707b25b60cafee3368f89827ac4
sksingh234 <shubhamkumar95817@gmail.com>
2024-10-06 13:01:29 +0530

    solving qs on array methods

---
+++ DELTA/Javascript/rise21ArrayMethodsAndSpread/assignmentArrayMethods/app.js
@@ -0,0 +1,21 @@
+/*  Qs 01
+check all the elements in array is multiple of 10 or not.
+*/
+
+// let nums = [10, 20, 30, 40, 50];
+// let result = nums.every((el) => (el % 10 == 0));
+// console.log(result);
+
+/*  Qs 02
+create a function to find minimum number in an array.
+*/
+
+// let nums = [10, 20, -30, 40, 50];
+// let ans = nums.reduce((min , el) => {
+//     if(el < min){
+//         return el;
+//     } else{
+//         return min;
+//     }
+// });
+// console.log(ans);
